b3c613aef88bdc88108f8a09315729715fc16261
Maksym Dolia <maksym.service@gmail.com>
2018-06-16 21:59:05 +0300

    Add initial Spring Boot setup

---
+++ src/main/java/me/dolia/lab/microserviceslab/MicroservicesLabApplication.java
@@ -0,0 +1,12 @@
+package me.dolia.lab.microserviceslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesLabApplication.class, args);
+	}
+}
