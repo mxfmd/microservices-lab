d25a090ddc2a27d85bdb81c3880d4a2df0149442
Maksym Dolia <maksym.service@gmail.com>
2018-06-21 10:14:24 +0300

    Add mock data for book storage

---
+++ book-service/src/main/java/me/dolia/lab/microserviceslab/Book.java
@@ -0,0 +1,26 @@
+package me.dolia.lab.microserviceslab;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+}
