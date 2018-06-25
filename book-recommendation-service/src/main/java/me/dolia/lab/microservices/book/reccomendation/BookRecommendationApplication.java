39ba462288040d6762dbaaaba98d279cdf8939f6
Maksym Dolia <maksym.service@gmail.com>
2018-06-25 13:01:54 +0300

    Add Spring Cloud Netflix Ribbon for client-side load-balancing

--- book-recommendation-service/src/main/java/me/dolia/lab/microservices/book/reccomendation/BookRecommendationApplication.java
+++ book-recommendation-service/src/main/java/me/dolia/lab/microservices/book/reccomendation/BookRecommendationApplication.java
@@ -3,6 +3,7 @@ package me.dolia.lab.microservices.book.reccomendation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,6 +18,7 @@ public class BookRecommendationApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate createRestTemplate() {
         return new RestTemplate();
     }
