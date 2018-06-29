600d42d1d81fc8cb2dfcf93798a43afbd6e4d90c
Maksym Dolia <maksym.service@gmail.com>
2018-06-29 18:41:14 +0300

    Externalize configuration to dedicated Configuration Service

--- book-recommendation-service/src/main/java/me/dolia/lab/microservices/book/reccomendation/BookRecommendationController.java
+++ book-recommendation-service/src/main/java/me/dolia/lab/microservices/book/reccomendation/BookRecommendationController.java
@@ -1,6 +1,7 @@
 package me.dolia.lab.microservices.book.reccomendation;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,18 @@ import java.util.Random;
 @RestController
 public class BookRecommendationController {
 
+    private final int maxBookId;
     private final BookServiceClient bookServiceClient;
 
-    public BookRecommendationController(BookServiceClient bookServiceClient) {
+    public BookRecommendationController(@Value("${maxBookId}") int maxBookId, BookServiceClient bookServiceClient) {
+        this.maxBookId = maxBookId;
         this.bookServiceClient = bookServiceClient;
     }
 
     @HystrixCommand(fallbackMethod = "reliable")
     @GetMapping(path = "book-recommendation")
     public Book recommendBook() {
-        int id = new Random().nextInt(3) + 1;
+        int id = new Random().nextInt(maxBookId) + 1;
         return bookServiceClient.getBookById((long) id).getContent();
     }
 
