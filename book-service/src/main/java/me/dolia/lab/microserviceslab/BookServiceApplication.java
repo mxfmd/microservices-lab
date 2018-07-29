8f10e28ba1bd1d571a60f485388919735c83ea79
Maksym Dolia <maksym.service@gmail.com>
2018-07-29 12:06:26 +0300

    Add Liquibase to promote DB changes to Book Service

--- book-service/src/main/java/me/dolia/lab/microserviceslab/BookServiceApplication.java
+++ book-service/src/main/java/me/dolia/lab/microserviceslab/BookServiceApplication.java
@@ -1,29 +1,14 @@
 package me.dolia.lab.microserviceslab;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BookServiceApplication {
 
-	@Autowired
-	private BookRepository bookRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner initializeBookStorage() {
-		return args -> {
-			bookRepository.save(new Book("Clean Architecture: A Craftsman's Guide to Software Structure and Design", "Robert Cecil Martin"));
-			bookRepository.save(new Book("Cracking the Coding Interview", "Gayle Laakmann McDowell"));
-			bookRepository.save(new Book("Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future", "Ashlee Vance"));
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
 }
