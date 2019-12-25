68477e8258f06cf7832a046007d31f75f3db9ceb
Maksym Dolia <MaksymDolia@users.noreply.github.com>
2019-12-25 13:38:08 +0200

    Add smoke test for Eureka server (#7)

--- discovery-service/src/test/java/me/dolia/lab/microservices/discovery/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/me/dolia/lab/microservices/discovery/DiscoveryServiceApplicationTests.java
@@ -1,16 +1,35 @@
 package me.dolia.lab.microservices.discovery;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+    properties = "logging.level.org.springframework.boot.test.web.client=trace",
+    webEnvironment = WebEnvironment.RANDOM_PORT
+)
 public class DiscoveryServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @LocalServerPort
+  private int port;
+  @Autowired
+  private TestRestTemplate rest;
 
+  @Test
+  public void eurekaEndpointIsAvailable() {
+    var url = "http://localhost:" + port + "/";
+
+    var response = rest.getForEntity(url, String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
 }
